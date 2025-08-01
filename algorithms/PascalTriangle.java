20fe5a672ef893d6cddc5f34475dad02cb820c7b
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-08-02 01:38:53 +0200

    First resolved task

--- algorithms/PascalTriangle.java
+++ algorithms/PascalTriangle.java
@@ -1,15 +1,22 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> pascalTrianble = new ArrayList<>();
-        for (int = 0; i < 30; i ++) {
-            List<Integer> auxiliarList = new ArrayList<>();
-            auxiliarList.set(0, 1)
-            auxiliarList.set(auxiliarList.size(), 1) ;
+        List<List<Integer>> pascalTriangle = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List auxiliarList = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                auxiliarList.add(1);
+            }
+            pascalTriangle.add(auxiliarList);
+        }
+
+        for (int i = 2; i < numRows; i++) {
             for (int j = 1; j < i; j++) {
-                auxiliarList = new ArrayList<>(i+1);
-                auxiliarList
+                var previousList = pascalTriangle.get(i - 1);
+                int valueCalculated = previousList.get(j - 1) + previousList.get(j);
+                pascalTriangle.get(i).set(j, valueCalculated);
             }
         }
+
         return pascalTriangle;
     }
 }
