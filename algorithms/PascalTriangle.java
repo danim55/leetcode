cacdcaa2af6072f47e926521e79da4b932cf27ea
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-08-02 01:12:02 +0200

    Create algorithms folder and new java file for first problem

---
+++ algorithms/PascalTriangle.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> pascalTrianble = new ArrayList<>();
+        for (int = 0; i < 30; i ++) {
+            List<Integer> auxiliarList = new ArrayList<>();
+            auxiliarList.set(0, 1)
+            auxiliarList.set(auxiliarList.size(), 1) ;
+            for (int j = 1; j < i; j++) {
+                auxiliarList = new ArrayList<>(i+1);
+                auxiliarList
+            }
+        }
+        return pascalTriangle;
+    }
+}
