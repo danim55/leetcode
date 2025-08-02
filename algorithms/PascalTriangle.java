26ed3918541cb2ad1172ac82d860e77018b38e17
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-08-02 18:45:46 +0200

    Add second solution (improved) in java and format the code

--- algorithms/PascalTriangle.java
+++ algorithms/PascalTriangle.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
+
+    // First solution
+    public List<List<Integer>> generateFirstTry(int numRows) {
         List<List<Integer>> pascalTriangle = new ArrayList<>();
         for (int i = 0; i < numRows; i++) {
             List auxiliarList = new ArrayList<>();
@@ -18,5 +24,24 @@ class Solution {
         }
 
         return pascalTriangle;
+    }
+
+    // Final solution
+    public List<List<Integer>> generateSecondTry(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        triangle.add(Arrays.asList(1));
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prev = triangle.get(i - 1);
+            row.add(1);
+            for (int j = 1; j < i; j++) {
+                row.add(prev.get(j - 1) + prev.get(j));
+            }
+            row.add(1);
+            triangle.add(row);
+        }
+
+        return triangle;
     }
 }
