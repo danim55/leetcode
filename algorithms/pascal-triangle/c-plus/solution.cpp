3f74b1cb313c91eb82b2e89de57f02832698f5f2
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-08-09 17:22:14 +0200

    WIP

--- algorithms/pascal-triangle/c-plus/solution.cpp
+++ algorithms/pascal-triangle/c-plus/solution.cpp
@@ -3,5 +3,8 @@ class Solution {
         vector<vector<int>> triangle;
         for(int i = 0; i < numRows; i++){
             vector<int> row(i+1, 1);
+            for(int j = 1; j < i; j++) {
+                row[j] = pascal[i-1][j-1] + pascal[i-1][j]; 
+            }
         } 
 };
