224b2476e534228e65e832d64a2549a01b8b7a21
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-08-11 18:40:39 +0200

    WIP

--- algorithms/merge-sorted-array/java/Solution.java
+++ algorithms/merge-sorted-array/java/Solution.java
@@ -1,6 +1,18 @@
+
+import java.util.Arrays;
+
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] combined = ArrayUtils.addAll(nums1, nums2);
-        
+        int[] combined = Arrays.copyOf(nums1, nums1.length + nums2.length);
+        System.arraycopy(nums2, 0, combined, nums1.length, nums2.length);
+        for (int i = 1; i < combined.length; i++) {
+            int element = combined[i];
+            int previousElement = combined[i - 1];
+            if (element < previousElement) {
+                combined[i - 1] = element;
+                combined[i] = previousElement;
+            }
+            System.out.println(combined.toString());
+        }
     }
 }
