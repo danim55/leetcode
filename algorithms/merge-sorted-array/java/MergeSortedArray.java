a010ebe71e59f246b1c1b66df6cc09fbd251c04a
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-08-12 10:12:22 +0200

    Simple solution with complexity O(m+n)log(m+n))

--- algorithms/merge-sorted-array/java/MergeSortedArray.java
+++ algorithms/merge-sorted-array/java/MergeSortedArray.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
 
 class Solution {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+        System.out.print(Arrays.toString(nums1));
     }
 
     public static void main(String args[]) {
--- algorithms/merge-sorted-array/java/Solution.java
+++ algorithms/merge-sorted-array/java/Solution.java
@@ -2,17 +2,9 @@
 import java.util.Arrays;
 
 class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] combined = Arrays.copyOf(nums1, nums1.length + nums2.length);
-        System.arraycopy(nums2, 0, combined, nums1.length, nums2.length);
-        for (int i = 1; i < combined.length; i++) {
-            int element = combined[i];
-            int previousElement = combined[i - 1];
-            if (element < previousElement) {
-                combined[i - 1] = element;
-                combined[i] = previousElement;
-            }
-            System.out.println(combined.toString());
-        }
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+        System.out.print(Arrays.toString(nums1));
     }
 }
