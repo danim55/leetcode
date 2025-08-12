8cbd21178d4df5b085562d4dda99c95029f3eb3d
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-08-12 09:56:50 +0200

    Rearrange folders

---
+++ algorithms/merge-sorted-array/java/MergeSortedArray.java
@@ -0,0 +1,12 @@
+
+class Solution {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+    }
+
+    public static void main(String args[]) {
+        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+        int[] nums2 = { 2, 5, 6 };
+        merge(nums1, 3, nums2, 3);
+    }
+}
