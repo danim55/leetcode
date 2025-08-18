dd0633f79c702ac3bace350d1b54e6c624bac089
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-08-19 00:01:05 +0200

    C solution

---
+++ algorithms/remove-duplicated-sorted-2/c-plus/solution.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        int n = nums.size();
+        int j = 1;
+        int counter = 1;
+
+        for (int i = 1; i < n; i++)
+        {
+            if (nums[i - 1] == nums[i])
+            {
+                counter++;
+            }
+            else
+            {
+                counter = 1;
+            }
+            if (counter <= 2)
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+};
