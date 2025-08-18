91ca534ff031fcb20caf52b97db6da79b1220883
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-08-18 23:47:26 +0200

    WIP

--- algorithms/remove-duplicated-sorted-2/java/Test.java
+++ algorithms/remove-duplicated-sorted-2/java/Test.java
@@ -2,20 +2,19 @@
 public class Test {
     public static int removeDuplicates(int[] nums) {
         int length = nums.length;
-        int counter = 0;
-        int changes = 0;
+        int j = 1;
+        int counter = 1;
 
-        for (int i = 1; i < length; i++) {
-            counter++;
-            if (nums[i] != nums[i - 1]) {
-                if (counter > 1) {
-                    nums[i - 1] = nums[i];
-                    changes++;
-                }
-                counter = 0;
+        for (int i = 0; i < length; i++) {
+            if (nums[i - 1] == nums[i]) {
+                counter++;
+            }
+            if (counter <= 2) {
+
             }
         }
-        return changes;
+
+        return counter;
     }
 
     public static void main(String args[]) {
