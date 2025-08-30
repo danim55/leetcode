ac22f9bc6194d0e2e22082d11ed2d9a467ede481
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-08-30 23:29:39 +0200

    Java sorted approach improved

--- 150-essential/majority-element/java/Solution.java
+++ 150-essential/majority-element/java/Solution.java
@@ -1,18 +1,10 @@
+
+import java.util.Arrays;
+
 class Solution {
     public int majorityElement(int[] nums) {
-        int counter = 0;
-        int number = nums[0];
-        int length = nums.length;
+        int n = nums.length;
         Arrays.sort(nums);
-        for (int i = 1; i < length; i++) {
-            if(nums[i]==nums[i-1]){
-                counter++;
-            }
-            if(counter >= length/2){
-                number = nums[i];
-                break;
-            }
-        }
-        return number;
+        return nums[n / 2];
     }
 }
