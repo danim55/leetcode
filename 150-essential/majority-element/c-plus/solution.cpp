a5bf4c7a17dc13a7ea175fac7fb343724587f405
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-08-31 00:05:37 +0200

    Upgrade c plus solution

--- 150-essential/majority-element/c-plus/solution.cpp
+++ 150-essential/majority-element/c-plus/solution.cpp
@@ -1,8 +1,26 @@
-class Solution {
+class Solution
+{
 public:
-    int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        return nums[n/2];
+    int majorityElement(vector<int> &nums)
+    {
+        int counter = 0;
+        int candidate = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (counter == 0)
+            {
+                candidate = nums[i];
+            }
+
+            if (candidate == nums[i])
+            {
+                counter++;
+            }
+            else
+            {
+                counter--;
+            }
+        }
     }
 };
