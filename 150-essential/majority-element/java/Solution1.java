b7a03dbacf20727dd063b8ade7431f6368f00e18
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-08-31 00:00:49 +0200

    WIP

--- 150-essential/majority-element/java/Solution1.java
+++ 150-essential/majority-element/java/Solution1.java
@@ -1,8 +1,13 @@
 
-import java.util.Arrays;
-
 class Solution {
     public int majorityElement(int[] nums) {
+        int counter = 0;
+        int candidate = 0;
 
+        for (int i = 0; i < nums.length; i++) {
+            if (counter == 0) {
+                candidate = nums[i];
+            }
+        }
     }
 }
