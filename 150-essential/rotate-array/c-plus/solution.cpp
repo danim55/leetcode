836ccfbc39daed3513faba6e9c479f41ffc7fb5e
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-01 22:52:20 +0200

    C++ solution 100%, not O(1) space

--- 150-essential/rotate-array/c-plus/solution.cpp
+++ 150-essential/rotate-array/c-plus/solution.cpp
@@ -4,12 +4,12 @@ public:
     void rotate(vector<int> &nums, int k)
     {
         int n = nums.size();
-        vector<int> rotated;
+        vector<int> rotated(n);
 
         for (int i = 0; i < n; i++)
         {
             rotated[(i + k) % n] = nums[i];
         }
-        nums = vector;
+        nums = rotated;
     }
 };
