e2770b0b655c65aa9f44f6bc15f46c7fbc1aa002
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-08 16:42:58 +0200

    WIP java

--- 150-essential/best-time-buy/java/Solution.java
+++ 150-essential/best-time-buy/java/Solution.java
@@ -3,11 +3,13 @@ class Solution {
         int n = prices.length;
         int profit = 0;
         int difference;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n - 1; i++) {
             difference = prices[i + 1] - prices[i];
             if (profit < difference) {
                 profit = difference;
             }
+            System.out.println(profit);
+            System.out.println(difference);
         }
         return profit;
     }
