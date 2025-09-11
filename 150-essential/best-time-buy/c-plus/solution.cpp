3fc0c9c7e7f50d37724660c2a9c7537df8238eb9
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-11 12:39:14 +0200

    WIP

--- 150-essential/best-time-buy/c-plus/solution.cpp
+++ 150-essential/best-time-buy/c-plus/solution.cpp
@@ -1,6 +1,24 @@
-class Solution {
+class Solution
+{
 public:
-    int maxProfit(vector<int>& prices) {
-        
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        int min = prices[0];
+        int profit = 0;
+        int difference;
+        for (int i = 1; i < n; i++)
+        {
+            difference = prices[i] - min;
+            if (prices[i] < min)
+            {
+                min = prices[i];
+            }
+            if (difference > profit)
+            {
+                profit = difference;
+            }
+        }
+        return profit;
     }
 };
