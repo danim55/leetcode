fd0948dd19389f3919b5de217c52484343dfaae6
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-14 12:46:44 +0200

    Java: WIP

--- 150-essential/jump-game/java/Solution.java
+++ 150-essential/jump-game/java/Solution.java
@@ -2,12 +2,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        if (n == 1) return true;
+        if (n == 1)
+            return true;
+        if (nums[0] == 0 || nums[n - 2] == 0)
+            return false;
         for (int i = 0; i < n; i++) {
-            if (nums[i] > n - 1 || nums[i] > n - i - 1) {
+            if (nums[i] >= n - 1 || nums[i] >= n - i - 1) {
                 return true;
-            } else if (nums[i] == 0){
-                break;
             }
         }
         return false;
