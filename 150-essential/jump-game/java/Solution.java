6fabe07a0881ab101f67ca2b545af7c47aacba07
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-14 12:41:35 +0200

    Java: WIP

--- 150-essential/jump-game/java/Solution.java
+++ 150-essential/jump-game/java/Solution.java
@@ -1,9 +1,15 @@
 
-import java.util.stream.IntStream;
-
 class Solution {
     public boolean canJump(int[] nums) {
-        int sum = IntStream.of(nums).sum();
-        return sum > nums.length * 2;
+        int n = nums.length;
+        if (n == 1) return true;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > n - 1 || nums[i] > n - i - 1) {
+                return true;
+            } else if (nums[i] == 0){
+                break;
+            }
+        }
+        return false;
     }
 }
