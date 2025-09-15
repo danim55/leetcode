b05fdf1a22c6af13e25297d37ac3c5b584c48fb0
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-15 14:13:36 +0200

    Java WIP

--- 150-essential/jump-game/java/Solution.java
+++ 150-essential/jump-game/java/Solution.java
@@ -2,17 +2,23 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        if (n == 1)
-            return true;
-        if (nums[0] == 0)
-            return false;
+        if (n == 1) return true;
+        int maxPosition = 0;
+        int currentMaxPosition = 0;
+        int difference;
         for (int i = 0; i < n; i++) {
-            if (nums[i] >= n - 1 || nums[i] >= n - i - 1) {
-                return true;
+            System.out.println("Num value: " + nums[i]);
+            currentMaxPosition = nums[i] + i;
+            difference = currentMaxPosition - maxPosition;
+            System.out.println("Current max position: " + currentMaxPosition);
+            if (difference > 0) {
+                maxPosition = currentMaxPosition;
             }
+            if (maxPosition <= i) {
+                return false;
+            }
+            System.out.println("Max position: " + maxPosition);
         }
-        if (nums[n - 2] == 0)
-            return false;
-        return false;
+        return maxPosition >= n;
     }
 }
