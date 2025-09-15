c5f76b5520f9199cccfe004b41a0c771c4373d27
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-15 14:18:42 +0200

    Java Solution

--- 150-essential/jump-game/java/Solution.java
+++ 150-essential/jump-game/java/Solution.java
@@ -4,21 +4,21 @@ class Solution {
         int n = nums.length;
         if (n == 1) return true;
         int maxPosition = 0;
-        int currentMaxPosition = 0;
+        int currentMaxPosition;
         int difference;
         for (int i = 0; i < n; i++) {
-            System.out.println("Num value: " + nums[i]);
             currentMaxPosition = nums[i] + i;
             difference = currentMaxPosition - maxPosition;
-            System.out.println("Current max position: " + currentMaxPosition);
             if (difference > 0) {
                 maxPosition = currentMaxPosition;
+            }
+            if (maxPosition >= n-1) {
+                return true;
             }
             if (maxPosition <= i) {
                 return false;
             }
-            System.out.println("Max position: " + maxPosition);
         }
-        return maxPosition >= n;
+        return maxPosition >= n-1;
     }
 }
