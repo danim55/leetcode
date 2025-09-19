5fb185c136c9d9192de52905551956c4a14e0a9b
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-19 07:26:07 +0200

    WIP

--- 150-essential/jump-game-ii/java/Solution.java
+++ 150-essential/jump-game-ii/java/Solution.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class Solution {
@@ -8,7 +9,11 @@ class Solution {
         maxLeap.add(0);
 
         for (int i = 0; i < nums.length; i++) {
-
+            int min = Collections.min(maxLeap);
+            if (nums[i] > min) {
+                maxLeap.remove(maxLeap.indexOf(min));
+                maxLeap.add(nums[i]);
+            }
         }
         return 0;
     }
