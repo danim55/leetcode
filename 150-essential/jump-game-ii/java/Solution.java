2dc79e7a2ed65a77770c4d4bcde58c7766b293bf
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-19 07:21:06 +0200

    WIP

--- 150-essential/jump-game-ii/java/Solution.java
+++ 150-essential/jump-game-ii/java/Solution.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public int jump(int[] nums) {
-        int leap = 0;
-        return leap;
+        List<Integer> maxLeap = new ArrayList<>();
+        maxLeap.add(0);
+        maxLeap.add(0);
+
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+        return 0;
     }
 }
