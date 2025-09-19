0f39214c036c5fb1495b6f04f4b3bdb09e4ffbdc
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-19 07:31:20 +0200

    WIP

--- 150-essential/jump-game-ii/java/Solution.java
+++ 150-essential/jump-game-ii/java/Solution.java
@@ -1,20 +1,17 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 class Solution {
     public int jump(int[] nums) {
-        List<Integer> maxLeap = new ArrayList<>();
-        maxLeap.add(0);
-        maxLeap.add(0);
-
-        for (int i = 0; i < nums.length; i++) {
-            int min = Collections.min(maxLeap);
-            if (nums[i] > min) {
-                maxLeap.remove(maxLeap.indexOf(min));
-                maxLeap.add(nums[i]);
-            }
+        int n = nums.length;
+        int total = 0;
+        int count = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i > n; i++) {
+            total += nums[i];
+            count++;
+            if (total >= n - 1)
+                break;
         }
-        return 0;
+        return count;
     }
 }
