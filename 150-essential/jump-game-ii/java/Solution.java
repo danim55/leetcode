36132e743262bc58823a3419fe549a543c4b5507
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-20 09:43:53 +0300

    WIP

--- 150-essential/jump-game-ii/java/Solution.java
+++ 150-essential/jump-game-ii/java/Solution.java
@@ -1,18 +1,5 @@
-import java.util.Arrays;
-
 class Solution {
     public int jump(int[] nums) {
-        int n = nums.length;
-        int total = 0;
-        int count = 0;
-        Arrays.sort(nums);
-        for (int i = n - 1; i > 0; i--) {
-            System.out.println(nums[i]);
-            total += nums[i];
-            count++;
-            if (total >= n - 1)
-                break;
-        }
-        return count;
+        return 0;
     }
 }
