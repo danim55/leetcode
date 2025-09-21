72a35a4b6cb85553ae801e8222ef421dd5283545
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-21 17:48:00 +0300

    WIP: dynamic solution

--- 150-essential/jump-game-ii/java/Solution.java
+++ 150-essential/jump-game-ii/java/Solution.java
@@ -1,5 +1,22 @@
+
+import static java.lang.Integer.max;
+
 class Solution {
     public int jump(int[] nums) {
+        int smallest = 0;
+        int n = nums.length;
+        int end = 0;
+        int far = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            far = max(far, i + nums[i]);
+
+            if (i == end) {
+                smallest += 1;
+
+            }
+        }
+
         return 0;
     }
 }
