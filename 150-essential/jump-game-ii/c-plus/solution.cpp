acaa98fc219f4b3ac7093010f23e1fcd6ab7a671
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-23 18:57:08 +0300

    Cplus: WIP

--- 150-essential/jump-game-ii/c-plus/solution.cpp
+++ 150-essential/jump-game-ii/c-plus/solution.cpp
@@ -6,10 +6,17 @@ public:
         int n = nums.size();
         int far = 0;
         int end = 0;
+        int smallest = 0;
 
         for (int i = 0; i < n - 1; i++)
         {
-            
+            if (i == end)
+            {
+                smallest += 1;
+                end = far;
+            }
         }
+
+        return smallest;
     }
 }
