cbb96bec0dc51c55eab7ba09678d2165ad860bd2
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-24 22:15:08 +0300

    Java solution

--- 150-essential/roman-to-integer/java/Solution.java
+++ 150-essential/roman-to-integer/java/Solution.java
@@ -3,10 +3,14 @@ class Solution {
         int integerValue = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            switch (c) {
+            char current = s.charAt(i);
+            char next = 0;
+            if (i < s.length() - 1) {
+                next = s.charAt(i+1);
+            }
+            switch (current) {
                 case 'I' -> {
-                    if (s.charAt(i + 1) == 'V' || s.charAt(i + 1) == 'X') {
+                    if (next == 'V' || next == 'X') {
                         integerValue -= 1;
                     } else {
                         integerValue += 1;
@@ -14,14 +18,20 @@ class Solution {
                 }
                 case 'V' -> integerValue += 5;
                 case 'X' -> {
-                    if (s.charAt(i + 1) == 'L' || s.charAt(i + 1) == 'C') {
+                    if (next == 'L' || next == 'C') {
                         integerValue -= 10;
                     } else {
                         integerValue += 10;
                     }
                 }
                 case 'L' -> integerValue += 50;
-                case 'C' -> integerValue += 100;
+                case 'C' -> {
+                    if (next == 'D' || next == 'M') {
+                        integerValue -= 100;
+                    } else {
+                        integerValue += 100;
+                    }
+                }
                 case 'D' -> integerValue += 500;
                 case 'M' -> integerValue += 1000;
                 default -> {
