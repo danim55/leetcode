07a21d5feca330d4ca35d5b74dfdd393ee3d301a
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-25 20:36:42 +0300

    WIP

--- 150-essential/roman-to-integer/java/Solution1.java
+++ 150-essential/roman-to-integer/java/Solution1.java
@@ -1,5 +1,22 @@
+import java.util.Map;
+
 class Solution {
     public int romanToInt(String s) {
-        return 0;
+        Map<Character, Integer> map = Map.of(
+                'I', 1, 'V', 5, 'X', 10,
+                'L', 50, 'C', 100, 'D', 500, 'M', 1000);
+
+        int intValue = 0;
+        intValue += s.charAt(0);
+        for (int i = 1; i < s.length(); i++) {
+
+            char character = s.charAt(i);
+
+            if (character < s.charAt(i - 1)) {
+
+            }
+        }
+
+        return intValue;
     }
 }
