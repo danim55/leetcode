6d3a64d2977d247f2defeb1857b927247a227068
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-26 20:21:16 +0300

    WIP

--- 150-essential/h-index/java/Solution.java
+++ 150-essential/h-index/java/Solution.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
+
 class Solution {
     public int hIndex(int[] citations) {
         int hIndex = 0;
+        int currentValue = 0;
+        int counter = 0;
+
+        Arrays.sort(citations);
+
+        for (int i = 0; i < citations.length; i++) {
+            
+        }
+
         return hIndex;
     }
 }
