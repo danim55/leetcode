ef72ab51ceb64bdf308c5b2a391786a63c5e4baa
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-26 20:14:57 +0300

    WIP

--- 150-essential/h-index/java/Solution.java
+++ 150-essential/h-index/java/Solution.java
@@ -1,20 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public int hIndex(int[] citations) {
         int hIndex = 0;
-        int citationsNumber = citations.length;
-        int candidate = 0;
-        List<Integer> currentArray = new ArrayList<>();
-
-        for (int i = 0; i < citationsNumber; i++) {
-            currentArray.add(citations[i]);
-            if (Math.round(currentArray.stream().mapToDouble(a -> a).average().getAsDouble()) > currentArray.size()) {
-                hIndex = candidate;
-            }
-            candidate++;
-        }
         return hIndex;
     }
 }
