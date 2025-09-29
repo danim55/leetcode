4bca9a649ec5968bf94ae24300b9984c4be755ee
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-29 19:57:06 +0300

    WIP: bruteforce

--- 150-essential/h-index/java/Solution.java
+++ 150-essential/h-index/java/Solution.java
@@ -1,17 +1,24 @@
-import java.util.Arrays;
-
 class Solution {
     public int hIndex(int[] citations) {
         int hIndex = 0;
-        Arrays.sort(citations);
+        int numPapers = 0;
 
-        // [3,0,6,1,5]
-        // [0,1,3,5,6]
+        for (int j = 1; j < citations.length + 1; j++) {
 
-        for (int i = 0; i < citations.length; i++) {
-            if (citations[i] >= i + 1) {
-                hIndex = citations[i];
+            for (int i = 0; i < citations.length; i++) {
+                if (citations[i] > j) {
+                    System.out.println("Citation: " + citations[i]);
+                    System.out.println("J: " + j);
+                    System.out.println("Num papers: " + numPapers);
+                    numPapers++;
+                }
+                if (numPapers >= j) {
+                    hIndex = j;
+                    numPapers = 0;
+                    break;
+                }
             }
+
         }
 
         return hIndex;
