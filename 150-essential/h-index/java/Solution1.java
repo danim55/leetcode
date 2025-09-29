fdcacbdf0661323013429a69e2b0b408b92dd17a
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-09-29 21:09:08 +0300

    WIP: O(n) solution

--- 150-essential/h-index/java/Solution1.java
+++ 150-essential/h-index/java/Solution1.java
@@ -1,6 +1,25 @@
 class Solution {
     public int hIndex(int[] citations) {
         int hIndex = 0;
+        int[] numberOfCitations = new int[citations.length + 1];
+
+        for (int i = 0; i < numberOfCitations.length; i++) {
+            if (citations[i] > citations.length) {
+                numberOfCitations[numberOfCitations.length - 1]++;
+            } else {
+                numberOfCitations[citations[i]]++;
+            }
+        }
+
+        System.out.println(String.valueOf(numberOfCitations));
+
+        int counter = 0;
+        for (int j = numberOfCitations.length; j > 0; j--) {
+            counter += numberOfCitations[j];
+            if (counter >= j) {
+                hIndex = j;
+            }
+        }
 
         return hIndex;
     }
