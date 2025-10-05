77aef7ae800024f169c23369f637f7892b4e7bbd
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-10-05 09:17:20 +0300

    Add remove and insert methods, and declare a HashMap array

--- 150-essential/get-random-o1/java/Solution.java
+++ 150-essential/get-random-o1/java/Solution.java
@@ -1,14 +1,30 @@
+
+import java.util.HashMap;
+
 class RandomizedSet {
 
+    private final HashMap<Integer, Integer> randomNumbers;
+
     public RandomizedSet() {
+        this.randomNumbers = new HashMap<>();
     }
 
     public boolean insert(int val) {
-
+        if (randomNumbers.containsKey(val)) {
+            return false;
+        } else {
+            randomNumbers.put(val, val);
+            return true;
+        }
     }
 
     public boolean remove(int val) {
-
+        if (randomNumbers.containsKey(val)) {
+            randomNumbers.remove(val);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public int getRandom() {
