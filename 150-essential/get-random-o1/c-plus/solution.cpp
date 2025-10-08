0677f7ff5b39e1e3d9efc0fec0629fd52af281ee
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-10-08 21:20:43 +0300

    C plus implementation

--- 150-essential/get-random-o1/c-plus/solution.cpp
+++ 150-essential/get-random-o1/c-plus/solution.cpp
@@ -20,18 +20,18 @@ class RandomizedSet {
     }
 
     bool remove(int val) {
-        if ( valuesIndex.find(val) == valuesIndex.end()) {
+        if (valuesIndex.find(val) == valuesIndex.end()) {
             return false;
         }
 
         int indexToRemove = valuesIndex.at(val);
         // Change the value of the index to delete by the current last item of
         // the list
-        valuesIndex[indexToRemove] = values.at(values.size() - 1);
+        valuesIndex[values.back()] = indexToRemove;
         // Erase the value received from the map
         valuesIndex.erase(val);
         // Move the value to be removed to the end of the list
-        values[indexToRemove] = values[values.size() - 1];
+        values[indexToRemove] = values.back();
         values.pop_back();
 
         return true;
