714156e70c76c2201f6b2f125d11b9fafb825738
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-10-10 22:22:29 +0300

    Solution

--- 150-essential/length-last-word/java/Solution1.java
+++ 150-essential/length-last-word/java/Solution1.java
@@ -5,10 +5,15 @@ class Solution {
         for (int i = s.length() - 1; i >= 0; i--) {
             if (s.charAt(i) != ' ') {
                 length++;
-            } else {
-                length = 0;
             }
 
+            if (s.charAt(i) == ' ') {
+                condition = true;
+            }
+
+            if (s.charAt(i) == ' ' && condition && length > 0) {
+                break;
+            }
         }
         return length;
     }
