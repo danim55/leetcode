aefe5c7031790a57b0f464841d1f04a04c9ecf4e
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-10-11 22:19:09 +0300

    Remove wrong solution

--- 150-essential/length-last-word/java/Solution.java
+++ 150-essential/length-last-word/java/Solution.java
@@ -1,20 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int endOfWord = s.length() - 1;
         int length = 0;
-        boolean word = false;
+        boolean condition = false;
         for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) != ' ' && !word) {
-                word = true;
-                endOfWord = i;
-                System.out.println(endOfWord);
+            if (s.charAt(i) != ' ') {
+                length++;
             }
-            System.out.println(s.charAt(i));
-            System.out.println(word);
-            if (word && s.charAt(i) == ' ') {
-                length = s.substring(i, endOfWord).length();
-                System.out.println(length);
-                System.out.println(s.substring(i,endOfWord));
+
+            if (s.charAt(i) == ' ') {
+                condition = true;
+            }
+
+            if (s.charAt(i) == ' ' && condition && length > 0) {
                 break;
             }
         }
