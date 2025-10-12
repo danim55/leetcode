6842113f7c05a4a666611fd454f7b22b70e4fdc0
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-10-12 19:25:01 +0300

    First attempt

---
+++ 150-essential/product-of-array/java/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] answer = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            int temp = nums[i];
+            nums[i] = 1;
+            for (int j = 0; j < nums.length; j++) {
+                answer[i] *= nums[j];
+            }
+            nums[i] = temp;
+        }
+
+        return answer;
+    }
+}
