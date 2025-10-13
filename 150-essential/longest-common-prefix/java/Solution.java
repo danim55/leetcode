96bfcf86d8de89c8ae86a41816c612044be9d329
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-10-13 22:08:04 +0300

    WIP: java

---
+++ 150-essential/longest-common-prefix/java/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String common = "";
+
+        for (int i = 0; i < strs.length; i++) {
+
+        }
+        return common;
+    }
+}
