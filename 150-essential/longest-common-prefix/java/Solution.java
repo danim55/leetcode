62ccb7e0dfc231010cbe2ca36a0537077143f744
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-10-16 20:29:40 +0300

    WIP

--- 150-essential/longest-common-prefix/java/Solution.java
+++ 150-essential/longest-common-prefix/java/Solution.java
@@ -1,15 +1,28 @@
+import java.util.Arrays;
+
 class Solution {
-    public String longestCommonPrefix(String[] v) {
-        StringBuilder ans = new StringBuilder();
-        Arrays.sort(v);
-        String first = v[0];
-        String last = v[v.length-1];
-        for (int i=0; i<Math.min(first.length(), last.length()); i++) {
-            if (first.charAt(i) != last.charAt(i)) {
-                return ans.toString();
+    public String longestCommonPrefix(String[] strs) {
+        String common = "";
+        String longerWord = "";
+        int index = 0;
+
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].length() > longerWord.length()) {
+                longerWord = strs[i];
+                index = i;
             }
-            ans.append(first.charAt(i));
         }
-        return ans.toString();
+        strs[index] = strs[strs.length - 1];
+        Arrays.copyOf(strs, strs.length - 1);
+        for (String str : strs) {
+            System.out.println(strs[0].length());
+            for (int j = 0; j < str.length() - 1; j++) {
+                if (str.charAt(j) != longerWord.charAt(j) || common.length() > j) {
+                    break;
+                }
+                common += str.charAt(j);
+            }
+        }
+        return common;
     }
 }
