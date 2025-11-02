b14a1973657b67316f51840f8d92108adabbae41
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-11-02 20:34:21 +0100

    testing

--- 150-essential/product-of-array/java/Solution.java
+++ 150-essential/product-of-array/java/Solution.java
@@ -1,17 +1,22 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] answer = new int[nums.length];
-
+        int[] output = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            int temp = nums[i];
-            nums[i] = 1;
-            answer[i] = 1;
-            for (int j = 0; j < nums.length; j++) {
-                answer[i] *= nums[j];
-            }
-            nums[i] = temp;
+            output[i] = 1;
         }
 
-        return answer;
+        int left = 1;
+        for (int i = 0; i < nums.length; i++) {
+            output[i] *= left;
+            left *= nums[i];
+        }
+
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            output[i] *= right;
+            right *= nums[i];
+        }
+
+        return output;        
     }
 }
