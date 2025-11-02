5d55e24d076ad265a0f73051bf892a05e795fb37
dmerinocano@gmail.com <dmerinocano@gmail.com>
2025-11-02 20:34:58 +0100

    testing

--- 150-essential/product-of-array/java/Solution.java
+++ 150-essential/product-of-array/java/Solution.java
@@ -5,11 +5,6 @@ class Solution {
             output[i] = 1;
         }
 
-        int left = 1;
-        for (int i = 0; i < nums.length; i++) {
-            output[i] *= left;
-            left *= nums[i];
-        }
 
         int right = 1;
         for (int i = nums.length - 1; i >= 0; i--) {
